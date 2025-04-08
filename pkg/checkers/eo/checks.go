8e310cdf35374a6909a43a95ffbc1aa25e2411cb
Marco Deicas <mdeicas@google.com>
2025-04-08 20:54:01 +0000

    Remove the old supplier check
    Signed-off-by: Marco Deicas <mdeicas@google.com>

--- pkg/checkers/eo/checks.go
+++ pkg/checkers/eo/checks.go
@@ -15,8 +15,6 @@
 package eo
 
 import (
-	"strings"
-
 	types "github.com/google/sbom-conformance/pkg/checkers/types"
 	"github.com/google/sbom-conformance/pkg/util"
 	v23 "github.com/spdx/tools-golang/spdx/v2/v2_3"
@@ -63,35 +61,6 @@ func MustHaveValidVersion(
 		issues = append(issues, issue)
 	}
 	return issues
-}
-
-func CheckGoogleSpellingInSupplier(
-	sbomPack *v23.Package,
-	spec, checkName string,
-) []*types.NonConformantField {
-	issues := make([]*types.NonConformantField, 0)
-	if sbomPack.PackageOriginator != nil {
-		if strings.ToLower(sbomPack.PackageOriginator.Originator) == "google" {
-			issue := wrongPackageOriginator(spec)
-			issue.CheckName = checkName
-			issues = append(issues, issue)
-		}
-	}
-	return issues
-}
-
-func wrongPackageOriginator(
-	spec string,
-) *types.NonConformantField {
-	e := "If the package originates from Google, " +
-		"the 'Package Originator' should be 'Google LLC'."
-	return &types.NonConformantField{
-		Error: &types.FieldError{
-			ErrorType: "formatError",
-			ErrorMsg:  e,
-		},
-		ReportedBySpec: []string{spec},
-	}
 }
 
 func MustHaveExternalReferences(
